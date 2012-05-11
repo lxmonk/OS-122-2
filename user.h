e0350fa5fc151d8548664861b32d981fa645bb53
Aviad Reich <avi.rei@gmail.com>
2012-05-11 11:46:08 +0300

    updated user.h

--- user.h
+++ user.h
@@ -36,3 +36,7 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+
+#define DEBUG_PRINT(fmt, ...)						\
+    do { if (T_A_DEBUG) printf(2, "<%s>:%d:[%s()]: " fmt "\n", __FILE__, \
+                               __LINE__, __func__, __VA_ARGS__); } while (0)
